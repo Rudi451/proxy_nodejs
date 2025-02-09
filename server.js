b2421abd49bee77486ef90de0cbdbfeee0ac5bed
Rodion Shapovalov <rodion.shap@gmail.com>
2025-02-09 15:11:06 +0100

    probe

--- server.js
+++ server.js
@@ -8,15 +8,21 @@ const server = http.createServer((req, res) => {
 
 	const targetMap = {
 		'my-yoga.work': 'http://localhost:7001',
-		'pingybot.com': 'http://localhost:5000',
+		'pingybot.com/*': 'http://localhost:5000',
+		// 'pingybot.com/.well-known/acme-challenge/KZ6x1S-MB47Njq-_a5_OR8TSZfP5SP4Ba0etjawsM1w'
 	};
 
 	if (targetMap[host]) {
-		proxy.web(req, res, {target: targetMap[host]}, (err) => {
-			console.error(`Fehler beim Proxying zu ${targetMap[host]}:`, err);
-			res.writeHead(404, {'Content-Type': 'text/plain'});
-			res.end('404 - Zielserver nicht erreichbar');
-		});
+		proxy.web(
+			req,
+			res,
+			{target: targetMap[host], changeOrigin: true},
+			(err) => {
+				console.error(`Fehler beim Proxying zu ${targetMap[host]}:`, err);
+				res.writeHead(404, {'Content-Type': 'text/plain'});
+				res.end('404 - Zielserver nicht erreichbar');
+			}
+		);
 	} else {
 		res.writeHead(404, {'Content-Type': 'text/plain'});
 		res.end('404 - Domain nicht gefunden');
