82b086842cb1e1eff263ce6c378aec2a9ed609df
Rodion Shapovalov <rodion.shap@gmail.com>
2025-02-15 18:14:54 +0100

    lk

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 
-//
-// Setup proxy server with forwarding
-//
-httpProxy
-	.createServer({
-		target: {
-			port: 7002,
-			host: 'localhost',
-		},
-		forward: {
-			port: 5000,
-			host: 'localhost',
-		},
-	})
-	.listen(7001);
+// Erstelle einen Proxy-Server
+const proxy = httpProxy.createProxyServer({});
+
+const server = http.createServer((req, res) => {
+	const host = req.headers.host;
+
+	// Routing basierend auf der Domain
+	if (host === 'my-yoga.work') {
+		proxy.web(req, res, {target: 'http://localhost:7002', secure: false});
+	} else if (host === 'pingybot.com') {
+		proxy.web(req, res, {target: 'http://localhost:5000', secure: false});
+	} else {
+		res.writeHead(404, {'Content-Type': 'text/plain'});
+		res.end('Domain nicht gefunden');
+	}
+});
+
+server.listen(7001, () => {
+	console.log('Proxy-Server läuft auf Port 7001');
+});
