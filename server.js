4b66929992a6c0cebac56afd08e231c97061d32e
Rodion Shapovalov <rodion.shap@gmail.com>
2025-02-15 18:10:12 +0100

    pf

--- server.js
+++ server.js
@@ -16,36 +16,3 @@ httpProxy
 		},
 	})
 	.listen(7001);
-
-//
-// Target Http Server
-//
-http
-	.createServer(function (req, res) {
-		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.write(
-			'request successfully proxied to: ' +
-				req.url +
-				'\n' +
-				JSON.stringify(req.headers, true, 2)
-		);
-		res.end();
-	})
-	.listen(7002);
-
-//
-// Target Http Forwarding Server
-//
-http
-	.createServer(function (req, res) {
-		console.log('Receiving forward for:', req.url);
-		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.write(
-			'request successfully forwarded to: ' +
-				req.url +
-				'\n' +
-				JSON.stringify(req.headers, true, 2)
-		);
-		res.end();
-	})
-	.listen(5000);
